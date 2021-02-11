366051b6f15237b5aed741c10ee653e24b6be816
rsoltys <radovan.soltys@gmail.com>
2021-02-11 13:44:43 +0100

    fix submitForm - asyncCloseOnSubmit enabled, asyncUpdate disabled

--- bootstrap_modal_forms/static/js/jquery.bootstrap.modal.forms.js
+++ bootstrap_modal_forms/static/js/jquery.bootstrap.modal.forms.js
@@ -57,8 +57,13 @@ https://github.com/trco/django-bootstrap-modal-forms
     // Submit form callback function
     var submitForm = function (settings) {        
         if (!settings.asyncUpdate) {
-            $(settings.modalForm).submit();
-        } else {          
+            if (!settings.asyncSettings.closeOnSubmit) {
+                $(settings.modalForm).submit();
+            }
+            else {
+                window.location.reload();
+            }
+        } else {
             var asyncSettingsValid = validateAsyncSettings(settings.asyncSettings);
             var asyncSettings = settings.asyncSettings;
 
@@ -78,7 +83,7 @@ https://github.com/trco/django-bootstrap-modal-forms
                             console.error("django-bootstrap-modal-forms: <body> element missing in your html.");
                         }
                         body.prepend(asyncSettings.successMessage);
-    
+
                         // Update page without refresh
                         $.ajax({
                             type: "GET",
@@ -87,12 +92,12 @@ https://github.com/trco/django-bootstrap-modal-forms
                             success: function (response) {
                                 // Update page
                                 $(asyncSettings.dataElementId).html(response[asyncSettings.dataKey]);
-    
+
                                 // Add modalForm to trigger element after async page update
                                 if (asyncSettings.addModalFormFunction) {
                                     asyncSettings.addModalFormFunction();
                                 }
-    
+
                                 if (asyncSettings.closeOnSubmit) {
                                     $(settings.modalID).modal("hide");
                                 } else {
@@ -116,7 +121,7 @@ https://github.com/trco/django-bootstrap-modal-forms
         if (!settings.successMessage) {
             missingSettings.push("successMessage");
             console.error("django-bootstrap-modal-forms: 'successMessage' in asyncSettings is missing.");
-        }        
+        }
         if (!settings.dataUrl) {
             missingSettings.push("dataUrl");
             console.error("django-bootstrap-modal-forms: 'dataUrl' in asyncSettings is missing.");
